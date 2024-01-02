538edbd7f594c4effd5fe6b207976e80c57998b6
Sorawit yoktlemtae <Sorawit_yok@cmu.ac.th>
2024-01-02 16:23:58 +0700

    com

--- lab13_1.cpp
+++ lab13_1.cpp
@@ -17,3 +17,49 @@ int main(){
     cout << "\nMin = " << B[5];
     return 0;
 }
+
+void stat(const double a[], int n, double b[]) {
+ 
+  double sum = 0.0;
+  for (int i = 0; i < n; i++) {
+    sum += a[i];
+  }
+  b[0] = sum / n;
+
+ 
+  double sum_of_squares = 0.0;
+  for (int i = 0; i < n; i++) {
+    sum_of_squares += (a[i] - b[0]) * (a[i] - b[0]);
+  }
+  b[1] = sqrt(sum_of_squares / n);
+
+ 
+  double product = 1.0;
+  for (int i = 0; i < n; i++) {
+    product *= a[i];
+  }
+  b[2] = pow(product, 1.0 / n);
+
+
+  double sum_of_reciprocals = 0.0;
+  for (int i = 0; i < n; i++) {
+    sum_of_reciprocals += 1.0 / a[i];
+  }
+  b[3] = n / sum_of_reciprocals;
+
+ 
+  b[4] = a[0];
+  for (int i = 1; i < n; i++) {
+    if (a[i] > b[4]) {
+      b[4] = a[i];
+    }
+  }
+
+
+  b[5] = a[0];
+  for (int i = 1; i < n; i++) {
+    if (a[i] < b[5]) {
+      b[5] = a[i];
+    }
+  }
+}
